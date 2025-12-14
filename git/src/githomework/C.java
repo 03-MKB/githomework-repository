02e83bf46430734c672827dff32261f0d27dd8e5
03-MKB <kyeongbin591@skuniv.ac.kr>
2025-12-14 17:56:07 +0900

    master c

--- git/src/githomework/C.java
+++ git/src/githomework/C.java
@@ -3,7 +3,7 @@ package githomework;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("git homework C");
-		System.out.println("git homework Alice C");
 		System.out.println("master change C");
+		
 	}
 }
