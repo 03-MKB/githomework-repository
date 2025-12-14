0ad51759838a2c2099ee14a7aac8ed204fbcf688
03-MKB <kyeongbin591@skuniv.ac.kr>
2025-12-14 18:39:52 +0900

    master bob A merge

--- git/src/githomework/A.java
+++ git/src/githomework/A.java
@@ -3,11 +3,7 @@ package githomework;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("git homework A");
-<<<<<<< HEAD
 		System.out.println("Alice A");
-=======
-		System.out.println("git homework Alice A");
 		System.out.println("change Bob A");
->>>>>>> refs/heads/Bob
 	}
 }
