d33c6136206352176f6544d06f1c55fe270aa383
03-MKB <kyeongbin591@skuniv.ac.kr>
2025-12-14 17:39:26 +0900

    Alice A C

--- git/src/githomework/C.java
+++ git/src/githomework/C.java
@@ -3,6 +3,6 @@ package githomework;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("git homework C");
-		System.out.println("git homework Alice C");
+		System.out.println("Alice C");
 	}
 }
